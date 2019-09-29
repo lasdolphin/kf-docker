da3c8f93932dc1ac1cfa095062de973ea961cbc6
Saed Alavinia <saed.alavinia@gemalto.com>
2019-09-29 10:40:11 -0400

    rc tagging

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -6,4 +6,4 @@ cp jq-github-release/jq-linux64 builddir/jq && chmod +x builddir/jq
 
 
 ## we have got the version of the kubectl in kubectl-release/kubectl-version
-wget -O builddir/kubectl "https://storage.googleapis.com/kubernetes-release/release/$(cat kubectl-release/kubectl-version)/bin/linux/amd64/kubectl" && chmod +x builddir/kubectl
+wget -q -O builddir/kubectl "https://storage.googleapis.com/kubernetes-release/release/$(cat kubectl-release/kubectl-version)/bin/linux/amd64/kubectl" && chmod +x builddir/kubectl
