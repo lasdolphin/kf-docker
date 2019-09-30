d3cfab6343c131f7dfde680c6097a9d66f8c1e7e
Saed Alavinia <saed.alavinia@gemalto.com>
2019-09-29 20:06:15 -0400

    Updating the way we get version of the latest gcloud

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -8,7 +8,9 @@ mv kf-cli-release/kf builddir/kf && chmod +x builddir/kf
 
 
 ## gettheing the release version by crawling the gcloud/release/release-version notes
-wget -q -O gcloud.tar.gz "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$(cat gcloud-release/release-version | egrep -m 1 -o '[0-9]+.[0-9]+.[0-9]+' | head -n 1)-linux-x86_64.tar.gz"
+GCLOUD_VERSION=$(cat gcloud-release/release-version | egrep -m 1 -o 'google-cloud-sdk-[0-9]+\.[0-
+9]+\.[0-9]' | sort -n | head -n 1)
+wget -q -O gcloud.tar.gz "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$GCLOUD_VERSION-linux-x86_64.tar.gz"
 tar xzf gcloud.tar.gz
 mv google-cloud-sdk/bin/gcloud builddir/gcloud
 mv google-cloud-sdk/bin/gsutil builddir/gsutil
